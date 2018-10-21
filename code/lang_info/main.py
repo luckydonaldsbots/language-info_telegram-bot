94f9edf7c290d186c9abee0399852edc7a6feea2
luckydonald <luckydonald@flutterb.at>
2018-10-21 23:28:44 +0200

    [anonymize|ban_warn|bass_boost|derpi|dumper|forked|forward_whitelist|hey_admin|lang_info|limiter|localize|mlfw|mustache|no_service|permalink|rules_rules|slides_robot|sticker_tag|travis|ya_tube] Switched out gitinfo with luckydonald-utils version, to allow url checks.

--- code/lang_info/main.py
+++ code/lang_info/main.py
@@ -6,23 +6,25 @@ from pytgbot import Bot
 from pytgbot.api_types.receivable.media import Audio
 from pytgbot.api_types.receivable.peer import User
 from pytgbot.api_types.receivable.updates import Message
+from teleflask import Teleflask
 from teleflask.messages import HTMLMessage
+from luckydonaldUtils.exceptions import assert_type_or_raise
+from luckydonaldUtils.tg_bots.gitinfo import version_bp, version_tbp
 
 from .langs import l
 from .secrets import API_KEY, URL_HOSTNAME, URL_PATH
-from luckydonaldUtils.exceptions import assert_type_or_raise
-import re
 
 __author__ = 'luckydonald'
 logger = logging.getLogger(__name__)
 logging.add_colored_handler(level=logging.DEBUG)
 
-from teleflask import Teleflask
 app = Flask(__name__)
-
+app.register_blueprint(version_bp)
 # sentry = add_error_reporting(app)
+
 bot = Teleflask(API_KEY, hostname=URL_HOSTNAME, hostpath=URL_PATH, hookpath="/income/{API_KEY}")
 bot.init_app(app)
+bot.register_tblueprint(version_tbp)
 
 assert_type_or_raise(bot.bot, Bot)
 
